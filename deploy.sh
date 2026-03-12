854ab6ee2bb88632ac62908a87f61489fd8f0df4
joyce <joyce_tankiatsy@msi-global.com.sg>
2026-03-12 10:30:59 +0800

    Update deploy.sh to always trigger redeploy

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ cp data/*.json public/data/
 
 echo "Committing and pushing..."
 git add data/ public/data/
-git commit -m "Update Jira data $(date +%Y-%m-%d)" || echo "No changes to commit"
+git commit -m "Update Jira data $(date +%Y-%m-%d)" || git commit --allow-empty -m "Redeploy $(date +%Y-%m-%d)"
 git push
 
 echo "Done! Site will update in ~1 minute at https://joyce-msi.github.io/mmsp/"
